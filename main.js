9268b4bc315ae629053ebddd119355a1b8b2b0c1
Practical Code Academy <67136508+PracticalCodeAcademy@users.noreply.github.com>
2020-10-06 10:57:59 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+// initiate constants for document elements 
+
+const currencyForm = document.getElementById('currency-form');
+const fromSelect = document.getElementById('from-select');
+const toSelect = document.getElementById('to-select');
+const currencyInput = document.getElementById('currency-input');
+const resultModal = document.querySelector('#result-modal .modal-content')
+const btnShowResults = document.getElementById('btn-show-results');
+const MsgContainer = document.getElementById('msg-container');
+const fromInput = document.getElementById('from-input');
+const toInput = document.getElementById('to-input');
+const factorInput = document.getElementById('factor-input');
+const saveChanges = document.getElementById('save-changes');
+const addBtn = document.getElementById('add-btn');
+
+
+const countriesList = countries;
